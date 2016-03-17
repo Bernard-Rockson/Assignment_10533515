ced6423a7095183177620f852107f8ef35986bc7
Bernard-Rockson <bernard.rockson.bb@gmail.com>
2016-03-17 09:44:42 -0700

    Hello, my assignment 2 is ready.

---
+++ CountingMachineRevisited.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10533515;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NANA
+ */
+public class CountingMachineRevisited {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int userFrom;
+        int userTo;
+        int userBy;
+        
+        System.out.print("Count from: ");
+        userFrom=input.nextInt();
+        System.out.print("Count to: ");
+        userTo=input.nextInt();
+        System.out.print("Count by: ");
+        userBy=input.nextInt();
+        
+        for(int i=userFrom; i<=userTo; i=i+userBy){
+            System.out.print(i+" ");
+        }
+        System.out.println("\n");
+    }
+    
+}
+

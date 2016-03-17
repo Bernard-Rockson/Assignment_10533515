34e7468f6a0a8707f47561f56dcb2f6b359dd716
Bernard-Rockson <bernard.rockson.bb@gmail.com>
2016-03-17 10:05:30 -0700

    Hello, my assignment 2 is ready.

---
+++ TenTimes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10533515;
+
+/**
+ *
+ * @author NANA
+ */
+public class TenTimes {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println(i+". Mr. Mitchell is cool.");
+        }
+    }
+    
+}
+

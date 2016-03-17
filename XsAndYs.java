3f620de9417e04f96513592906e285532fe91a5a
Bernard-Rockson <bernard.rockson.bb@gmail.com>
2016-03-17 10:06:31 -0700

    Hello, my assignment 2 is ready.

---
+++ XsAndYs.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10533515;
+
+/**
+ *
+ * @author NANA
+ */
+public class XsAndYs {
+    public static void main(String[] args) {
+        System.out.println("X\n-------------");
+        for(double i=-10.0; i<=10.0; i=i+0.5){
+            System.out.println(i+"\t"+(Math.pow(i, 2)));
+        }
+    }
+}
+
